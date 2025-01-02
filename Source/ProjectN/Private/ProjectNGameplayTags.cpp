55b70a04e45f337b86aaa148726f602603bed4aa
Nikolai Chedurov <Effet666@gmail.com>
2025-01-02 23:47:10 +0100

    Initial commit

---
+++ Source/ProjectN/Private/ProjectNGameplayTags.cpp
@@ -0,0 +1,11 @@
+// N Chedurov All Rights Reserved
+
+
+#include "ProjectNGameplayTags.h"
+
+namespace ProjectNGameplayTags
+{
+	/**** Input Tags ****/
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look")
+}
---
+++ Source/ProjectN/Public/ProjectNGameplayTags.h
@@ -0,0 +1,12 @@
+// N Chedurov All Rights Reserved
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace ProjectNGameplayTags
+{
+	/**** Input Tags ****/
+	PROJECTN_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	PROJECTN_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
