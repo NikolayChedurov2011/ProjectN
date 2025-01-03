89bc0474e9f69eb1d5a8e0ef52f7a3c9b80013a5
NChedurov <n.chedurov@fstr.tech>
2025-01-03 16:50:39 +0100

    Added map and Input data assets

---
+++ Source/ProjectN/Private/DataAssets/InputConfig/DataAsset_InputConfig.cpp
@@ -0,0 +1,5 @@
+// N Chedurov All Rights Reserved
+
+
+#include "DataAssets/InputConfig/DataAsset_InputConfig.h"
+
---
+++ Source/ProjectN/Public/DataAssets/InputConfig/DataAsset_InputConfig.h
@@ -0,0 +1,17 @@
+// N Chedurov All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DataAsset_InputConfig.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTN_API UDataAsset_InputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
